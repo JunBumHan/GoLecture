ad3d1c23d6f75364edb64bfbca26f478a6ca542f
JunBumHan <2hanjunbum6@gmail.com>
2023-03-11 17:30:24 +0900

    Add if.go

---
+++ 03-control-flow-statements/if.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	a, b := 5, 10
+
+	if a > b {
+		fmt.Println("a > b true")
+	} else {
+		fmt.Println("a < b ture")
+	}
+
+	// 쇼트서킷 조심!
+	if a > b && a < b {
+		fmt.Println("쇼트서킷 조심!")
+	} else {
+		fmt.Println("쇼트서킷 조심!")
+	}
+}
