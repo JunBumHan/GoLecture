4335b710cfe30bb82c06d17d7af2d7499ef4c79f
JunBumHan <2hanjunbum6@gmail.com>
2023-03-11 17:37:17 +0900

    Add 조건식

--- 03-control-flow-statements/if.go
+++ 03-control-flow-statements/if.go
@@ -11,6 +11,11 @@ func main() {
 		fmt.Println("a < b ture")
 	}
 
+	// 조건식에는 무조권 bool타입만이 와야함!
+	// if 1 {
+	// 	fmt.Println("True")
+	// }
+
 	// 쇼트서킷 조심!
 	if a > b && a < b {
 		fmt.Println("쇼트서킷 조심!")
