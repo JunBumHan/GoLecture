9b97b9cfd97d245aa1363a9d10d22aecffbb6267
JunBumHan <2hanjunbum6@gmail.com>
2023-03-12 20:52:03 +0900

    Add func literal

--- 04-function/function.go
+++ 04-function/function.go
@@ -1,6 +1,8 @@
 package main
 
-func add(value1, value2 int) int { // == func add(value1, value2 int) int {}
+import "fmt"
+
+func Add(value1, value2 int) int { // == func add(value1, value2 int) int {}
 	return value1 + value2
 }
 
@@ -25,6 +27,49 @@ func Divide(a, b int) (result int, success bool) {
 	return
 }
 
+// defer
+func Hello() {
+	defer fmt.Println("A")
+	defer fmt.Println("B")
+	fmt.Println("A!")
+}
+
+// 함수 타입 변수
+func add(a, b int) int {
+	return a + b
+}
+
+func mul(a, b int) int {
+	return a * b
+}
+
+func getOperator(op string) func(int, int) int {
+	if op == "+" {
+		return add
+	} else if op == "*" {
+		return mul
+	} else {
+		return nil
+	}
+
+}
+
+// 함수 리터럴 2
+func writeHello(writer func(string)) {
+	writer("Hello world")
+}
+
 func main() {
 
+	// 함수 리터럴 1
+	i := 0
+	f := func() {
+		i += 10
+	}
+
+	f()
+
+	writeHello(func(msg string) {
+		fmt.Printf(msg)
+	})
 }
